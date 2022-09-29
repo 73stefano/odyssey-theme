1bec95304c32db94ec6d64e8d77e20047cf1800a
Stefano Pica <stefanopica@Mac-mini-di-Stefano.local>
2022-09-29 19:59:13 +0200

    modified:   .DS_Store 	new file:   theme/public/assets/images/home/Lorena-Sarra.jpeg 	new file:   theme/public/assets/images/home/Prisco-Santoro-2.jpeg 	new file:   theme/public/assets/images/home/Prisco-Santoro.png 	new file:   theme/public/assets/images/home/Ramona-Veneri.jpg 	modified:   theme/src/components/sections/heros/HomeHeroSection.astro 	modified:   theme/src/config/footer.js 	modified:   theme/src/pages/404.astro 	modified:   theme/src/pages/biografia.astro 	modified:   theme/src/pages/company/contact.astro 	modified:   theme/src/pages/galleria.astro

--- theme/src/config/footer.js
+++ theme/src/config/footer.js
@@ -1,80 +1,117 @@
 export const footerSocials = [
   {
     name: 'Twitter',
-    url: 'https://twitter.com/littlesticksdev',
+    url: 'https://twitter.com/',
     icon: "mdi:twitter",
   },
   {
     name: 'Youtube',
-    url: 'https://youtube.com/c/LittleSticks',
+    url: 'https://youtube.com/c/',
     icon: "mdi:youtube",
   },
+//   {
+//     name: 'Github',
+//     url: 'https://github.com/littlesticks',
+//     icon: "mdi:github",
+//   },
+
+//   {
+//     name: 'Discord',
+//     url: 'https://littlesticks.dev/discord',
+//     icon: "ic:baseline-discord",
+//   },
   {
-    name: 'Github',
-    url: 'https://github.com/littlesticks',
-    icon: "mdi:github",
-  },
+    name: 'Facebook',
+    url: 'https://twitter.com/',
+    icon: "mdi:facebook",
+	},
   {
-    name: 'Discord',
-    url: 'https://littlesticks.dev/discord',
-    icon: "ic:baseline-discord",
-  },
+    name: 'Instagram',
+    url: 'https://twitter.com/',
+    icon: "mdi:instagram",
+  }
 ]
 
 export const footerLists = [
 	{
-		title: 'Landing Pages',
 		items: [
 			{
-				title: 'Landing Page 1',
-				slug: '/landing-pages/landing-1',
+				title: 'Legal Page',
+				slug: '/company/legal.astro',
 			},
-			{
-				title: 'Landing Page 2',
-				slug: '/landing-pages/landing-2',
-			},
-      // {
-			// 	title: 'Landing Page 3',
-			// 	slug: '/landing-pages/landing-3',
-			// },
-		],
+		]
 	},
 	{
-		title: 'Company',
 		items: [
 			{
-				title: 'About',
-				slug: '/company/about',
-			},
-			{
-				title: 'Blog',
-				slug: '/blog',
-			},
-			{
-				title: 'Contact',
-				slug: '/company/contact',
-			},
-		],
+				title: 'Cookie Page',
+				slug: '/company/legal.astro'
+			}
+		]
+		 
 	},
 	{
-		title: 'Theme',
 		items: [
 			{
-				title: 'Get Started',
-				slug: '/theme/get-started',
-			},
-			{
-				title: 'Style Guide',
-				slug: '/theme/style-guide',
-			},
-      {
-        title: 'Theme Setup',
-        slug: '/theme/theme-setup',
-      },
-      {
-        title: 'Customizing Odyssey',
-        slug: '/theme/customizing-odyssey',
-      }
-		],
-	},
-];
+				
+			}
+		]
+		 
+	}
+]
+// 	{
+// 		title: 'Landing Pages',
+// 		items: [
+// 			{
+// 				title: 'Landing Page 1',
+// 				slug: '/landing-pages/landing-1',
+// 			},
+// 			{
+// 				title: 'Landing Page 2',
+// 				slug: '/landing-pages/landing-2',
+// 			},
+//       {
+// 				title: 'Landing Page 3',
+// 				slug: '/landing-pages/landing-3',
+// 			},
+// 		],
+// 	},
+// 	{
+// 		title: 'Company',
+// 		items: [
+// 			{
+// 				title: 'About',
+// 				slug: '/company/about',
+// 			},
+// 			{
+// 				title: 'Blog',
+// 				slug: '/blog',
+// 			},
+// 			{
+// 				title: 'Contact',
+// 				slug: '/company/contact',
+// 			},
+// 		],
+// 	},
+// 	{
+// 		title: 'Theme',
+// 		items: [
+// 			{
+// 				title: 'Get Started',
+// 				slug: '/theme/get-started',
+// 			},
+// 			{
+// 				title: 'Style Guide',
+// 				slug: '/theme/style-guide',
+// 			},
+//       {
+//         title: 'Theme Setup',
+//         slug: '/theme/theme-setup',
+//       },
+//       {
+//         title: 'Customizing Odyssey',
+//         slug: '/theme/customizing-odyssey',
+//       }
+// 		],
+// 	},
+// ];
